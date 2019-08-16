ada2f7328200d5fecb496184dac4363a12d9da63
zizheng huang <u6248330@anu.edu.au>
2019-08-16 21:25:11 +1000

    add google map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
+declare var google: { maps: { Map: new (arg0: any, arg1: { center: { lat: number; lng: number; }; zoom: number; }) => void; }; };
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterContentInit {
+  map: any;
+  @ViewChild('mapElement') MapElement: { nativeElement: any; }
+  constructor() {
 
-  constructor() {}
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+  ngAfterContentInit(): void{
+    this.map = new google.maps.Map(
+      this.MapElement.nativeElement,
+      {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      }
+    );
+  }
 
 }
