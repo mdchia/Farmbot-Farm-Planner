7926b846030e866f7bd27c4429bfcdb3e1b40279
Patrick <patrick@iisys.com.au>
2019-08-27 21:26:58 +1000

    Added polygon drawing and zoom/rotate controls

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,25 @@
 
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import * as MapBoxDraw from '@mapbox/mapbox-gl-draw';
 
 @Component({
   selector: 'src-app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-
 export class HomePage implements OnInit {
 
   map: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/outdoors-v9';
+  style = 'mapbox://styles/mapbox/streets-v11';
   lat = -35.28;
   lng = 149.13;
 
+  draw: MapBoxDraw;
+
   constructor() {
-    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set('pk.eyJ1IjoiaHp6enoiLCJhIjoiY2p6cG1hOGFoMDE3dzNtbjQ2ZHpiZmI4cSJ9.5PMTx2nHNouS-uTeNO8FVQ');
+    Object.getOwnPropertyDescriptor(mapboxgl, 'accessToken')
+    .set('pk.eyJ1IjoiaHp6enoiLCJhIjoiY2p6cG1hOGFoMDE3dzNtbjQ2ZHpiZmI4cSJ9.5PMTx2nHNouS-uTeNO8FVQ');
   }
 
   ngOnInit() {
@@ -30,10 +33,18 @@ export class HomePage implements OnInit {
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
-      zoom: 13,
+      zoom: 11,
       center: [this.lng, this.lat]
     });
 
-    console.log(this.map);
+    this.draw = new MapBoxDraw({
+      displayControlsDefault: false,
+      controls: {
+        polygon: true,
+        trash: true
+      }
+    });
+    this.map.addControl(this.draw);
+    this.map.addControl(new mapboxgl.NavigationControl());
     }
   }
